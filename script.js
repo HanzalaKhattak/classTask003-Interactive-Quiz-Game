8a9730ce2d99b2473e7a39dbb3bdbca13dd917ac
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-09-06 01:11:42 +0500

    show question section added in script file

---
+++ script.js
@@ -0,0 +1,30 @@
+var questions = [
+    {
+        question: "What is the capital of Russia?",
+        choice: ["Islamabad", "Washington", "Mosccow", "Kabul"],
+        answer: 2
+    },
+    {
+        question: "Who was the undisputed UFC Champion with a 29 - 0 Wins Record?",
+        choices: ["Volkanowski", "Jones", "Lesner","Khabib"],
+        answer: 3
+    },
+    {
+        question: "What is our Planet's Name?",
+        choices:c["Mars", "Earth", "Jupiter", "Moon"],
+        answer: 1
+    }
+];
+
+
+// Function which will show us the questions
+var currentQuestionIndex = 0;
+
+function showQuestion(){
+    document.getElementById('question').textContent = questions[currentQuestionIndex].question;
+    document.getElementById('choice1').textContent = questions[currentQuestionIndex].choice[0];
+    document.getElementById('choice2').textContent = questions[currentQuestionIndex].choice[1];
+    document.getElementById('choice3').textContent = questions[currentQuestionIndex].choice[2];
+    document.getElementById('choice4').textContent = questions[currentQuestionIndex].choice[3];
+}
+showQuestion();
